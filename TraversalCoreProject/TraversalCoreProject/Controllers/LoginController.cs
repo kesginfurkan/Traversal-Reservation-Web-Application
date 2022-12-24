162b0c9a32ba2f05b6e0991d1b0dc8acaf8b3cb8
Hasan Furkan KESGİN <kesginfurkan97@gmail.com>
2022-12-24 18:50:21 +0300

    Added Identity, SignUp & SignIn template

---
+++ TraversalCoreProject/TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignIn()
+        //{
+        //    return View();
+        //}
+    }
+}
